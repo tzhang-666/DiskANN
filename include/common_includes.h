79ab3ec2c9fe7ccd6293f6a4773896960c53d17f
Tao Zhang <tzhang@microsoft.com>
2022-12-05 19:57:41 +0800

    ci

---
+++ include/common_includes.h
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include <algorithm>
+#include <atomic>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fcntl.h>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <random>
+#include <string.h>
+#include <sys/stat.h>
+#include <sstream>
+#include <time.h>
+#include <vector>
---
+++ include/neighbor.h
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include <cstddef>
+#include <mutex>
+#include <vector>
+#include "utils.h"
+
+namespace diskann {
+
+  struct Neighbor {
+    unsigned id;
+    float    distance;
+    bool     flag;
+
+    Neighbor() = default;
+    Neighbor(unsigned id, float distance, bool f)
+        : id{id}, distance{distance}, flag(f) {
+    }
+
+    inline bool operator<(const Neighbor &other) const {
+      return distance < other.distance ||
+          (distance == other.distance && id < other.id);
+    }
+
+    inline bool operator==(const Neighbor &other) const {
+      return (id == other.id);
+    }
+  };
+
+  static inline unsigned InsertIntoPool(Neighbor *addr, unsigned K,
+                                        Neighbor nn) {
+    // find the location to insert
+    unsigned left = 0, right = K - 1;
+    if (addr[left].distance > nn.distance) {
+      memmove((char *) &addr[left + 1], &addr[left], K * sizeof(Neighbor));
+      addr[left] = nn;
+      return left;
+    }
+    if (addr[right].distance < nn.distance) {
+      addr[K] = nn;
+      return K;
+    }
+    while (right > 1 && left < right - 1) {
+      unsigned mid = (left + right) / 2;
+      if (addr[mid].distance > nn.distance)
+        right = mid;
+      else
+        left = mid;
+    }
+    // check equal ID
+
+    while (left > 0) {
+      if (addr[left].distance < nn.distance)
+        break;
+      if (addr[left].id == nn.id)
+        return K + 1;
+      left--;
+    }
+    if (addr[left].id == nn.id || addr[right].id == nn.id)
+      return K + 1;
+    memmove((char *) &addr[right + 1], &addr[right],
+            (K - right) * sizeof(Neighbor));
+    addr[right] = nn;
+    return right;
+  }
+}  // namespace diskann
